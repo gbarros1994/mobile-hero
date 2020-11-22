c86d2410dfaf8f5fb0be6909af20a08d534b87f7
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2020-11-22 12:56:37 -0300

    load no scroll

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { View, Text, Image, TouchableOpacity, _View, Linking } from 'react-native';
-import * as MailComposer from 'expo-mail-composer';
+// import * as MailComposer from 'expo-mail-composer';
 
 import styles from './styles';
 
@@ -19,13 +19,13 @@ export default function Incidents() {
         navigation.goBack();
     }
 
-    function sendMail() {
-        MailComposer.composeAsync({
-            subject: `Heroi do caso: ${incident.title}`,
-            recipients: [incident.email],
-            body: message,
-        });
-    }
+    // function sendMail() {
+    //     MailComposer.composeAsync({
+    //         subject: `Heroi do caso: ${incident.title}`,
+    //         recipients: [incident.email],
+    //         body: message,
+    //     });
+    // }
 
     function sendWhatsapp() {
         Linking.openURL(`whatsapp://send?phone=5515997303725&text=${message}`);
@@ -39,24 +39,18 @@ export default function Incidents() {
                     <Feather name="arrow-left" size={28} color="#E82041" />
                 </TouchableOpacity>
             </View>
-
             <View style={styles.incident}>
                 <Text style={[styles.incidentProperty, {marginTop:0}]}>ONG:</Text>
                 <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
-
                 <Text style={styles.incidentProperty}>CASO:</Text>
                 <Text style={styles.incidentValue}>{incident.title}</Text>
-
                 <Text style={styles.incidentProperty}>VALOR:</Text>
                 <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</Text>
-
             </View>
-
             <View style={styles.contactBox}>
                 <Text style={styles.heroTitle}>Salve o dia!</Text>
                 <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
                 <Text style={styles.heroDescription}>Entre em contato</Text>
-
                 <View style={styles.actions}>
                     <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>WhatsApp</Text>
--- src/pages/incidents/index.js
+++ src/pages/incidents/index.js
@@ -12,6 +12,8 @@ import styles from './styles';
 export default function Incidents() {
     const [incidents, setIncidents] = useState([]);
     const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = false;
 
     const navigation = useNavigation();
 
@@ -20,9 +22,24 @@ export default function Incidents() {
     }
 
     async function loadIncidents() {
-        const response = await api.get('incidents');
-        setIncidents(response.data);
+        if (loading) {
+            return;
+        }
+
+        if (total > 0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true);
+
+
+        const response = await api.get('incidents', {
+            params: { page }
+        });
+        setIncidents([...incidents, ...response.data]);
         setTotal(response.headers['x-total-count']);
+        setLoading(page + 1);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -46,6 +63,8 @@ export default function Incidents() {
                 style={styles.incidentList}
                 showsVerticalScrollIndicator={false}
                 keyExtractor={incident => String(incident.id)}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
